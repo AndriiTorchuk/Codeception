9cf8e531dd2ccd6ed78590be6845c58113167a26
AndriiTorchuk <andriytv86@gmail.com>
2022-01-30 21:19:31 +0300

    Test

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -11,7 +11,27 @@ class FirstCest
     {
         $I->amOnPage('/');
         $I->wait(1); 
-        
+        $I->click(['link' => 'Карьера']);
+        $I->wait(1); 
+        $I->switchToNextTab(1);
+        $I->amOnUrl('https://career.netpeak.group/');
+        $I->wait(5);
+        $I->click(['link' => 'Я хочу работать в Netpeak']);
+        $I->wait(1); 
+        $I->attachFile('input[type=file]', 'a.png');
+        $I->wait(3);
+        $I->fillField('name', 'name');
+        $I->fillField('lastname', 'lastname');
+        $I->fillField('hiringe', '12345@gmail.com');
+        $I->fillField('phone', '123456789');
+        $I->selectOption('by','2000');
+        $I->selectOption('bm','января');
+        $I->selectOption('bd','1');
+        $I->wait(1); 
+        $I->click('Отправить анкету');
+        $I->wait(5); 
+        $I->click(['link' => 'Интернатура']);
+        $I->wait(3); 
         
         
 
